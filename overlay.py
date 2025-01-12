0ba5a2c624e2424f0c6b96b26acd2b479bb6b1cd
Dan Benbenisti <118917046+danben12@users.noreply.github.com>
2025-01-12 09:04:07 +0200

    Add files via upload

--- overlay.py
+++ overlay.py
@@ -5,6 +5,8 @@ from bokeh.models import Legend, LegendItem
 from bokeh.palettes import Category20
 from bokeh.models import HoverTool, TapTool
 from bokeh.models import ColumnDataSource
+from bokeh.layouts import column
+from bokeh.models import Range1d
 
 plot_types, chip_names = comparisons.dashborde()
 
@@ -117,5 +119,122 @@ def Initial_Density_Vs_Volume_overlay(plot_types,chip_names):
     scatter.add_layout(legend, 'right')
     scatter.legend.click_policy = 'hide'
     return scatter
+def Fraction_in_each_bin_overlay(plot_types,chip_names):
+    Fraction_in_each_bin=plot_types['Fraction_in_each_bin']
+    scatter = figure(title='Fraction of Population in Each Bin at Start and End of Simulation',
+                     x_axis_label='Bin Range', y_axis_label='Fraction of Population', output_backend="webgl",
+                     y_range=(0,100), width=1600, height=1200)
+    legend_items = []
+    colors = Category20[20]  # Use a palette with enough colors
+    for i, s in enumerate(Fraction_in_each_bin):
+        for j, renderer in enumerate(s.renderers):
+            renderer.visible = False
+            scatter.renderers.append(renderer)
+            if j == 0:
+                label = f'Start {chip_names[i]}'
+                renderer.glyph.line_color = colors[i * 2 + j % 2]
+                renderer.glyph.fill_color = colors[i * 2 + j % 2]
+            elif j == 1:
+                label = f'End {chip_names[i]}'
+                renderer.glyph.line_color = colors[i * 2 + j % 2]
+                renderer.glyph.fill_color = colors[i * 2 + j % 2]
+            legend_items.append(LegendItem(label=label, renderers=[renderer]))
+    legend = Legend(items=legend_items)
+    scatter.add_layout(legend, 'right')
+    scatter.legend.click_policy = 'hide'
+    return scatter
+def growth_curves_overlay(plot_types,chip_names):
+    growth_curves_lin = [plot.children[0] for plot in plot_types['growth_curves']]
+    growth_curves_log = [plot.children[1] for plot in plot_types['growth_curves']]
+    fig_linear=figure(title='Growth Curves', x_axis_label='Time', y_axis_label='Mean Count',
+               width=1600, height=1200, output_backend="webgl")
+    fig_linear.y_range = Range1d(0, 1e7)
+    fig_log=figure(title='Growth Log Scale', x_axis_label='Time', y_axis_label='Mean Count', width=1600, height=1200, output_backend="webgl", y_axis_type='log')
+    fig_log.y_range = Range1d(1, 1e7)
+    legend_items = []
+    for i, g in enumerate(growth_curves_lin):
+        for j in range(0, len(g.renderers), 2):
+            renderer1 = g.renderers[j]
+            renderer2 = g.renderers[j + 1] if j + 1 < len(g.renderers) else None
+            renderer1.visible = False
+            fig_linear.renderers.append(renderer1)
+            if renderer2:
+                renderer2.visible = False
+                fig_linear.renderers.append(renderer2)
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1, renderer2]))
+            else:
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1]))
+    legend = Legend(items=legend_items)
+    fig_linear.add_layout(legend, 'right')
+    fig_linear.legend.click_policy = 'hide'
+    legend_items = []
+    for i, g in enumerate(growth_curves_log):
+        for j in range(0, len(g.renderers), 2):
+            renderer1 = g.renderers[j]
+            renderer2 = g.renderers[j + 1] if j + 1 < len(g.renderers) else None
+            renderer1.visible = False
+            fig_log.renderers.append(renderer1)
+            if renderer2:
+                renderer2.visible = False
+                fig_log.renderers.append(renderer2)
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1, renderer2]))
+            else:
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1]))
+    legend = Legend(items=legend_items)
+    fig_log.add_layout(legend, 'right')
+    fig_log.legend.click_policy = 'hide'
+    return column(fig_linear,fig_log)
+def normalize_growth_curves_overlay(plot_types,chip_names):
+    normalize_growth_curves_lin = [plot.children[0] for plot in plot_types['normalize_growth_curves']]
+    normalize_growth_curves_log = [plot.children[1] for plot in plot_types['normalize_growth_curves']]
+    fig_linear = figure(title='Growth Curves', x_axis_label='Time', y_axis_label='Mean Count',
+                        width=1600, height=1200, output_backend="webgl")
+    fig_linear.y_range = Range1d(0, 1.3)
+    fig_log = figure(title='Growth Log Scale', x_axis_label='Time', y_axis_label='Mean Count', width=1600, height=1200,
+                     output_backend="webgl", y_axis_type='log')
+    fig_log.y_range = Range1d(0.0085, 1.3)
+    legend_items = []
+    for i, g in enumerate(normalize_growth_curves_lin):
+        for j in range(0, len(g.renderers), 2):
+            renderer1 = g.renderers[j]
+            renderer2 = g.renderers[j + 1] if j + 1 < len(g.renderers) else None
+            renderer1.visible = False
+            fig_linear.renderers.append(renderer1)
+            if renderer2:
+                renderer2.visible = False
+                fig_linear.renderers.append(renderer2)
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1, renderer2]))
+            else:
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1]))
+    legend = Legend(items=legend_items)
+    fig_linear.add_layout(legend, 'right')
+    fig_linear.legend.click_policy = 'hide'
+    legend_items = []
+    for i, g in enumerate(normalize_growth_curves_log):
+        for j in range(0, len(g.renderers), 2):
+            renderer1 = g.renderers[j]
+            renderer2 = g.renderers[j + 1] if j + 1 < len(g.renderers) else None
+            renderer1.visible = False
+            fig_log.renderers.append(renderer1)
+            if renderer2:
+                renderer2.visible = False
+                fig_log.renderers.append(renderer2)
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1, renderer2]))
+            else:
+                label = f"{g.legend.items[j // 2].label['value']} {chip_names[i]}"  # Keep the original label
+                legend_items.append(LegendItem(label=label, renderers=[renderer1]))
+    legend = Legend(items=legend_items)
+    fig_log.add_layout(legend, 'right')
+    fig_log.legend.click_policy = 'hide'
+    return column(fig_linear, fig_log)
+
+
 
 
